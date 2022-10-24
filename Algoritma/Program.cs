5b2dc2aec7261fad52bf6a9a71b461ecc09d6809
Nanang Puji <nanangpuji762@gmail.com>
2022-10-24 16:17:25 +0700

    Add project files.

---
+++ Algoritma/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        class Program
+        {
+            //Array to be searched
+            int[] arr = new int[20];
+            //number of elements in the array
+            int n;
+            //Get the number of elements to store in the array
+            int i;
+
+            public void input()
+
+        }
+    }
+}
